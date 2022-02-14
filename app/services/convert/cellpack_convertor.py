f43bf82f3415d469cb638b76f679a6b095c217d7
tangshilin <tangshilin@beidouapp.com>
2022-02-14 17:19:13 +0800

    生成电池组数据

--- app/services/convert/cellpack_convertor.py
+++ app/services/convert/cellpack_convertor.py
@@ -5,10 +5,11 @@ class CellPackConvertor:
 
     @staticmethod
     def __parse_str_to_json(value):
+        load_dict = None
         try:
-            return json.loads(value)
+            load_dict = json.loads(value)
         finally:
-            return None
+            return load_dict
 
     @staticmethod
     def __get_metric_value(item, metric):
@@ -41,6 +42,12 @@ class CellPackConvertor:
         return "number"
 
     @staticmethod
+    def __has_special_key(key):
+        if key in ["envTemp", "cellVol", "cellSoc"]:
+            return True
+        return False
+
+    @staticmethod
     def convert(items, metrics):
         useMetrics = metrics.split(",")
         useMetrics.insert(0, "ts")
@@ -53,5 +60,29 @@ class CellPackConvertor:
                     tmpDict[m] = [CellPackConvertor.__get_metric_value(item, m)]
         ret = []
         for key in tmpDict.keys():
-            ret.append({"name": key, "type": CellPackConvertor.__get_metric_type(key), "values": tmpDict[key]})
+            if CellPackConvertor.__has_special_key(key):
+                convert_items = CellPackConvertor.convert_special(key, tmpDict[key])
+                for it in convert_items:
+                    ret.append(it)
+            else:
+                ret.append({"name": key, "type": CellPackConvertor.__get_metric_type(key), "values": tmpDict[key]})
+        return ret
+
+    @staticmethod
+    def convert_special(key, datas):
+        tmpDict = {}
+        for data in datas:
+            items = CellPackConvertor.__parse_str_to_json(data)
+            i = 0
+            for item in items:
+                combineKey = key + str(i)
+                if combineKey in tmpDict.keys():
+                    tmpDict[combineKey].append(item)
+                else:
+                    tmpDict[combineKey] = [item]
+                i = i + 1
+
+        ret = []
+        for key in tmpDict.keys():
+            ret.append({"name": key, "type": "number", "values": tmpDict[key]})
         return ret
