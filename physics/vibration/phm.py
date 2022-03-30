b82d6c3bbc7a245bf0613f954a7b16ea970fddd8
tangshilin <tangshilin@beidouapp.com>
2022-03-30 17:14:38 +0800

    添加绘图

--- physics/vibration/phm.py
+++ physics/vibration/phm.py
@@ -3,6 +3,8 @@ import json
 import matplotlib.pyplot as plt
 from adjustText import adjust_text
 from matplotlib import patches as mpatches
+from mpl_toolkits.mplot3d import Axes3D
+
 from physics.vibration import cluster, mds
 import logging
 import httpx
@@ -44,52 +46,7 @@ def model_invoke(dataS, dimension):
             for el in elems:
                 df2.loc[el, 'color'] = dfnew.loc[idx, 'color']
         logging.info(f'1.MDS pos computed.')
-        # plot mds scatter chart
-        plt.figure(1)
-        plt.axes([0., 0., 1., 1.])
-        dispindices = list(range(objpos))  # only plot benchmark points.
-        plt.scatter(x=pos[dispindices, 0],
-                    y=pos[dispindices, 1],
-                    s=df2.loc[dispindices, 'age'],
-                    label=df2.loc[dispindices, 'color'],
-                    edgecolors=df2.loc[dispindices, 'color'],
-                    facecolors='none', marker='.', alpha=0.5, lw=1)
-        hnds = []
-        for idx, el in enumerate(dfnew['color']):
-            pop = mpatches.Patch(color=el, label=f'C: [{dfnew["cid"][idx]}], cnts:[{len(dfnew["vectors"][idx])}]')
-            hnds.append(pop)
-        plt.legend(handles=hnds, prop={'size': 6})
-        # label baseline points
-        # baseline should be the first points
-        # agelist = [24, 12, 24, 24, 6, ...]
-        # dfnew:
-        #       cid,       vectors
-        #        -1     [84,85,86,87, ...]
-        #         0     [156,157, ...]
-        #        ...
-        #         8     [0,1,2, ...]
-        # should label points: 0, 24, 36, 60
-        blcnts = 2
-        baselineclass = [0]
-        for idx, el in enumerate(agelist[0: blcnts]):
-            ps = baselineclass[idx] + el
-            baselineclass.append(ps)
-        # find baselineclass leader points in dfnew and
-        texts = []
-        for obj in baselineclass:  # 0, 24, 36, 60
-            for idx, vecs in enumerate(dfnew['vectors']):
-                if obj in vecs:
-                    txt = f'{obj} in C: [{dfnew["cid"][idx]}]'
-                    texts.append(plt.text(pos[obj, 0], pos[obj, 1], txt))
-        adjust_text(texts)
-        # label object points
-        plt.scatter(x=pos[objpos:, 0],
-                    y=pos[objpos:, 1],
-                    label=df2.loc[objpos:, 'color'],
-                    marker='*', s=300, color='k', alpha=0.5, lw=1)
-        for txt in list(range(objpos, len(datumn))):
-            plt.text(pos[txt, 0], pos[txt, 1], f'PT: {txt}', c='#000000')
-        plt.show()
+        model_draw(objpos, pos, df2, dfnew, agelist, datumn, dimension)
         logging.info('2.MDS plot finished.')
         df2.drop(df2.columns[list(range(len(df2.T) - 3))], axis=1, inplace=True)
         df2['shape'] = 0
@@ -117,3 +74,69 @@ def model_convert(inDatas):
             else:
                 outDatas[innerKey] = [inDatas[key][innerKey]]
     return outDatas
+
+
+def __draw_2d(objpos, pos, df2, dfnew, agelist, datumn):
+    # plot mds scatter chart
+    plt.figure(1)
+    plt.axes([0., 0., 1., 1.])
+    dispindices = list(range(objpos))  # only plot benchmark points.
+    plt.scatter(x=pos[dispindices, 0],
+                y=pos[dispindices, 1],
+                s=df2.loc[dispindices, 'age'],
+                label=df2.loc[dispindices, 'color'],
+                edgecolors=df2.loc[dispindices, 'color'],
+                facecolors='none', marker='.', alpha=0.5, lw=1)
+    hnds = []
+    for idx, el in enumerate(dfnew['color']):
+        pop = mpatches.Patch(color=el, label=f'C: [{dfnew["cid"][idx]}], cnts:[{len(dfnew["vectors"][idx])}]')
+        hnds.append(pop)
+    plt.legend(handles=hnds, prop={'size': 6})
+    blcnts = 2
+    baselineclass = [0]
+    for idx, el in enumerate(agelist[0: blcnts]):
+        ps = baselineclass[idx] + el
+        baselineclass.append(ps)
+    # find baselineclass leader points in dfnew and
+    texts = []
+    for obj in baselineclass:  # 0, 24, 36, 60
+        for idx, vecs in enumerate(dfnew['vectors']):
+            if obj in vecs:
+                txt = f'{obj} in C: [{dfnew["cid"][idx]}]'
+                texts.append(plt.text(pos[obj, 0], pos[obj, 1], txt))
+    adjust_text(texts)
+    # label object points
+    plt.scatter(x=pos[objpos:, 0],
+                y=pos[objpos:, 1],
+                label=df2.loc[objpos:, 'color'],
+                marker='*', s=300, color='k', alpha=0.5, lw=1)
+    for txt in list(range(objpos, len(datumn))):
+        plt.text(pos[txt, 0], pos[txt, 1], f'PT: {txt}', c='#000000')
+    plt.show()
+
+
+def __draw_3D(objpos, pos, df2, dfnew, agelist, datumn):
+    # plot mds scatter chart
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    dispindices = list(range(objpos))  # only plot benchmark points.
+    ax.scatter(xs=pos[dispindices, 0],
+               ys=pos[dispindices, 1],
+               zs=pos[dispindices, 2],
+               s=df2.loc[dispindices, 'age'],
+               label=df2.loc[dispindices, 'color'],
+               edgecolors=df2.loc[dispindices, 'color'],
+               facecolors='none', marker='.', alpha=0.5, lw=1)
+    hnds = []
+    for idx, el in enumerate(dfnew['color']):
+        pop = mpatches.Patch(color=el, label=f'C: [{dfnew["cid"][idx]}], cnts:[{len(dfnew["vectors"][idx])}]')
+        hnds.append(pop)
+    ax.legend(handles=hnds, prop={'size': 6})
+    plt.show()
+
+
+def model_draw(objpos, pos, df2, dfnew, agelist, datumn, dimension):
+    if dimension == 3:
+        __draw_3D(objpos, pos, df2, dfnew, agelist, datumn)
+    else:
+        __draw_2d(objpos, pos, df2, dfnew, agelist, datumn)
