3699cfce3c6394a1ad2126d0c7c1ab82cd9505e9
tangshilin <tangshilin@beidouapp.com>
2022-05-30 11:15:56 +0800

    fix bug

--- phmconfig/constants.py
+++ phmconfig/constants.py
@@ -73,7 +73,7 @@ URL_MS_CALL_RELATION = PHMMD_URL_PREFIX + "/api/v1/relation"
 # 数据资源
 URL_SJZY_API_PREFIX = cfg["url_sjzy_host"]
 # 从数据资源获取装备所有测点
-API_QUERY_EQUIP_INFO_WITH_MEASURE_POINT = URL_SJZY_API_PREFIX + "/api/equip/query_info_with_measure_point"
+API_QUERY_EQUIP_INFO_WITH_MEASURE_POINT = URL_SJZY_API_PREFIX + "/api/equip/query_equip_info_with_measure_point"
 # 从数据资源获取装备的测点数据
 API_QUERY_HISTORY_DATA = URL_SJZY_API_PREFIX + "/api/devices/query_history_data"
 
