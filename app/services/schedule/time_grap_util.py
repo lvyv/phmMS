13edbad01dc68226f6a8d1f8ed840e92c3853333
tangshilin <tangshilin@beidouapp.com>
2022-06-19 19:37:18 +0800

    去掉频繁日志打印

--- app/services/schedule/time_grap_util.py
+++ app/services/schedule/time_grap_util.py
@@ -10,6 +10,6 @@ class TimeGrapUtil:
         now_time = int(datetime.now().timestamp())
         if self.last_time == 0 or now_time - self.last_time > constants.CLICK_GRAP:
             self.last_time = now_time
-            logging.info("time grap is arrived, can click...")
+            # logging.info("time grap is arrived, can click...")
             return True
         return False
