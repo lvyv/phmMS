1239cc46386511d61bdcc0e050e2d8c6e1a66e47
tangshilin <tangshilin@beidouapp.com>
2022-07-01 19:13:54 +0800

    fix

--- app/services/validate/publicModelValidate.py
+++ app/services/validate/publicModelValidate.py
@@ -5,4 +5,6 @@ class PublicModelValidate:
             return False, '', ''
         devs = list(set(filter(None, equipCode.split(","))))
         metrics = list(set(filter(None, metrics.split(","))))
+        if len(devs) == 0 or len(metrics) == 0:
+            return False, '', ''
         return True, ",".join(dev for dev in devs), ",".join(metric for metric in metrics)
